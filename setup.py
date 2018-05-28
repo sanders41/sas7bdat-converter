33c56d051b0c071d5768f525064a75ad01e3d81d
Paul Sanders <psanders1@gmail.com>
2018-05-28 09:36:41 -0400

    Updated to v0.2.1 to fix file extension issue with directory converting.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ Conversions can be done on a single file, a batch of files, or a whole directory
 
 setup(
     name='sas7bdat_converter',
-    version='0.2.0',
+    version='0.2.1',
     author='Paul Sanders',
     author_email='psanders1@gmail.com',
     license='Apache 2.0',
