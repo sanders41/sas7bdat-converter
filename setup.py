f1c72113c2fc9ee46dd51f6068f9fe1f4904f17f
Paul Sanders <psanders1@gmail.com>
2019-07-04 18:25:16 -0400

    updated versions

--- setup.py
+++ setup.py
@@ -9,22 +9,23 @@ Conversions can be done on a single file, a batch of files, or a whole directory
 
 setup(
     name='sas7bdat_converter',
-    version='0.2.6',
+    version='0.2.7',
     author='Paul Sanders',
     author_email='psanders1@gmail.com',
     license='Apache 2.0',
     description='Convert sas7bdat files into other formats',
     long_description=long_description,
     url='https://github.com/sanders41/sas7bdat_converter',
-    download_url='https://github.com/sanders41/sas7bdat_converter/archive/v0.2.6.tar.gz',
-    install_requires=['pandas>=0.23.0',
-                      'XlsxWriter>=1.0.5'],
+    download_url='https://github.com/sanders41/sas7bdat_converter/archive/v0.2.7.tar.gz',
+    install_requires=['pandas>=0.24.2',
+                      'XlsxWriter>=1.2.0'],
     extras_require={
-        'test': ['xlrd>=1.1.0'],
+        'test': ['xlrd>=1.2.0'],
     },
     packages=find_packages(),
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent'
     ],
