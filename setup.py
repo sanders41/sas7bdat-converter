380946f218d8e3770d9ec31e15a02aa65815e838
Paul Sanders <psanders1@gmail.com>
2020-01-20 09:45:58 -0500

    long description update

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
+from pathlib import Path
 from setuptools import find_packages, setup
 
-long_description = '''
-Converts proprietary sas7bdat files from SAS into formats such as csv and XML useable by other programs. 
-Currently supported conversiaions are csv, Excel (xlsx format), json, Pandas DataFrame, and XML.
-
-Conversions can be done on a single file, a batch of files, or a whole directory.
-'''
+HERE = Path(__file__).parent
+README = (HERE.joinpath('README.md').read_text()
 
 setup(
     name='sas7bdat_converter',
@@ -14,7 +11,8 @@ setup(
     author_email='psanders1@gmail.com',
     license='Apache 2.0',
     description='Convert sas7bdat files into other formats',
-    long_description=long_description,
+    long_description=README,
+    long_description_content_type='text/markdown',
     url='https://github.com/sanders41/sas7bdat_converter',
     download_url='https://github.com/sanders41/sas7bdat_converter/archive/v0.3.1.tar.gz',
     packages=['sas7bdat_converter'],
