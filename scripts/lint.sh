add377ad59c41e52eca2d7e52f827f4b16eaee13
Paul Sanders <psanders1@gmail.com>
2020-11-01 13:52:37 -0500

    Moving CI to GitHub Actions

--- scripts/lint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-set -x
-
-mypy sas7bdat_converter
-isort sas7bdat_converter tests --check-only
-black sas7bdat_converter tests --check
-flake8 sas7bdat_converter tests
